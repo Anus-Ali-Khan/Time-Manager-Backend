4b6439c6c1c55d46dfad201850ff81eacea87ea4
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-05-08 11:37:22 +0500

    'backend'

---
+++ src/main/java/com/anuscode/TimeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anuscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuscode/TimeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
